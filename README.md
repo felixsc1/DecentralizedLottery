1eac3b434aa60d0e8ab8be3bfb0f1536e5e3ce40
Felix Schlegel <schlegel.felix@gmail.com>
2021-11-30 16:07:09 +0100

    implemented the calculate entrance fee function and its test.py

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Users can enter a lottery with ETH based on an USD fee. The price can be set to e.g. 50 USD and is internally converted to Ethereum.
+
+2. An admin will choose when the lottery is over. (Future extension: Let the lottery automatically open and close.)
+
+3. The lottery will select a random winner.
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,30 @@
-pragma solidity ^0.6.6;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
-    function enter() public {}
+    address[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
-    function getEntranceFee() public {}
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enter() public payable {
+        // 50$ minimum
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
 
     function startLottery() public {}
 
